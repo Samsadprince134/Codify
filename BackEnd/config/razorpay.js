405d59425af8a1d32ac42082908d3e1eabb7f7c6
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 19:23:16 +0530

    Update razorpay.js

--- BackEnd/config/razorpay.js
+++ BackEnd/config/razorpay.js
@@ -1,10 +1,16 @@
 const Razorpay = require("razorpay");
 require("dotenv").config();
 
+
+try{
 exports.instance = new Razorpay({
     key_id : process.env.RAZORPAY_KEY,
     key_secret : process.env.RAZORPAY_SECRET,
 
 })
+}
+catch(error){
+      console.error("Error initializing Razorpay:", error);
+}
 
 
