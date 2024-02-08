95fe3da683d8aeaf0699da8201b79f10fde2338f
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 21:59:27 +0530

    Update Course.js

--- BackEnd/routes/Course.js
+++ BackEnd/routes/Course.js
@@ -1,7 +1,12 @@
 // Import the required modules
 const express = require("express")
 const router = express.Router()
-
+const cors = require("cors");
+router.use(cors({
+  origin: 'https://codify-frontend-one.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // Import the Controllers
 
 // Course Controllers Import
