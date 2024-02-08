8a9125331e166eda16f94565778873cfc071fff3
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 21:59:41 +0530

    Update Contact.js

--- BackEnd/routes/Contact.js
+++ BackEnd/routes/Contact.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const cors = require("cors");
+router.use(cors({
+  origin: 'https://codify-frontend-one.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 const { contactUs } = require("../controllers/Contactus")
 
 router.post("/contactus", contactUs)
