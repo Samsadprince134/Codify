db46111ef53d29e78fba0ce80a062fe416e52177
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 22:15:43 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -16,6 +16,14 @@ app.use(cookieParser());
 const fileUpload = require("express-fileupload");
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://codify-frontend-one.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.use(
   fileUpload({
     useTempFiles: true,
