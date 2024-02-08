e869fced9654556abe50c0fa8b620460bcafe1c6
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 22:10:25 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -23,17 +23,12 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://codify-frontend-one.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
 
 
 app.use(
     cors({
-        origin : "https://codify-frontend-one.vercel.app/",
+        origin : "https://codify-frontend-one.vercel.app",
         credentials : true,
         methods : ["GET" , "POST" , "PUT" , "DELETE"],
 
