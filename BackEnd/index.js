c8f112715d7e148795fe1cf1c0ca4708ed110d2a
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 21:23:09 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -23,7 +23,12 @@ app.use(
   })
 );
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://codify-frontend-one.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.use(
