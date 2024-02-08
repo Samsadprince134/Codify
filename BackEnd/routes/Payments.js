774038a9ae7e3e4cffcc278ebfd51aacc81d519e
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-08 22:12:48 +0530

    Update Payments.js

--- BackEnd/routes/Payments.js
+++ BackEnd/routes/Payments.js
@@ -1,12 +1,7 @@
 // Import the required modules
 const express = require("express")
 const router = express.Router()
-const cors = require("cors");
-router.use(cors({
-  origin: 'https://codify-frontend-one.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
 
 const { capturePayment, verifySignature, sendPaymentSuccessEmail } = require("../controllers/Payments")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
