23a3cdfd4b5c50926e53273cbe41baabcc67ef86
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-02-09 00:14:13 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -17,14 +17,14 @@ const fileUpload = require("express-fileupload");
 
 
 
-function corsHandler() {
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://codify-frontend-one.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-}
+
 
 
 app.use(
